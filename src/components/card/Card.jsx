7f95ee0d15e44f460bf1035202072ffc6f4277da
Elspeth <elspeth@mardaljevic.org>
2024-08-14 10:06:52 +0100

    card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,31 @@
 import "./card.module.css";
 
 function Card() {
-  return <h1>Card</h1>;
+  const cards = [{img: "works", title:"Give us a call...", desc:`Call us and book in a "Design Consulation" on a date and time to suit you`}]
+  return (
+  
+     
+    <figure class="card">
+    <img class="card_img" src="/images/how-it-works-1.png" alt="image" />
+    <figcaption class="card_caption">
+        <h2 class="card_caption_title">Give us a call...</h2>
+        <p class="card_caption_desc">
+            Call us and book in a "Design Consulation" on a date
+            and time to suit you.
+        </p>
+    </figcaption>
+</figure>
+
+
+{
+  img: 
+  title:
+  desc:
+  }
+
+
+  )
 }
+
 
 export default Card;
